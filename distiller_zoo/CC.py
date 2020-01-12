6a3e77ab62145135f4355821d73fec21d1125e04
Yonglong Tian <yonglong@mit.edu>
2020-01-12 16:26:34 -0500

    Update CC.py

--- distiller_zoo/CC.py
+++ distiller_zoo/CC.py
@@ -6,8 +6,8 @@ import torch.nn as nn
 
 class Correlation(nn.Module):
     """Correlation Congruence for Knowledge Distillation, ICCV 2019.
-    The authors nicely shared the code with me. This is a modified version based on
-    the original authors' implementation. Credits go to the original author"""
+    The authors nicely shared the code with me. I restructured their code to be 
+    compatible with my running framework. Credits go to the original author"""
     def __init__(self):
         super(Correlation, self).__init__()
 
