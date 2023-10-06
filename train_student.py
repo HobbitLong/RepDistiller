4952ece0a5af01262582547cbc9bea0377c3f7a5
Geetika Vadali <123307246+geetHonve@users.noreply.github.com>
2023-10-06 21:44:43 +0530

    Update train_student.py

--- train_student.py
+++ train_student.py
@@ -9,7 +9,6 @@ import argparse
 import socket
 import time
 
-import matplotlib.pyplot as plt
 import tensorboard_logger as tb_logger
 import torch
 import torch.optim as optim
@@ -44,7 +43,7 @@ def parse_option():
     parser.add_argument('--save_freq', type=int, default=40, help='save frequency')
     parser.add_argument('--batch_size', type=int, default=64, help='batch_size')
     parser.add_argument('--num_workers', type=int, default=8, help='num of workers to use')
-    parser.add_argument('--epochs', type=int, default=4, help='number of training epochs')
+    parser.add_argument('--epochs', type=int, default=240, help='number of training epochs')
     parser.add_argument('--init_epochs', type=int, default=30, help='init training for two-stage methods')
 
     # optimization
@@ -141,6 +140,8 @@ def load_teacher(model_path, n_cls):
     return model
 
 
+# ...
+
 def main():
     best_acc = 0
 
@@ -168,11 +169,15 @@ def main():
     model_t = load_teacher(opt.path_t, n_cls)
     model_s = model_dict[opt.model_s](num_classes=n_cls)
 
-    data = torch.randn(2, 3, 32, 32).cpu()
+    data = torch.randn(2, 3, 32, 32).to('cuda')  # Moved to GPU
     model_t.eval()
     model_s.eval()
     feat_t, _ = model_t(data, is_feat=True)
     feat_s, _ = model_s(data, is_feat=True)
+
+    # Move tensors to CPU before converting to numpy
+    feat_t = [f.cpu() for f in feat_t]
+    feat_s = [f.cpu() for f in feat_s]
 
     module_list = nn.ModuleList([])
     module_list.append(model_s)
@@ -279,19 +284,15 @@ def main():
     # append teacher after optimizer to avoid weight_decay
     module_list.append(model_t)
 
-    # if torch.cuda.is_available():
-    #     module_list.cuda()
-    #     criterion_list.cuda()
-    #     cudnn.benchmark = True
+    if torch.cuda.is_available():
+        module_list.cuda()
+        criterion_list.cuda()
+        cudnn.benchmark = True
 
     # validate teacher accuracy
     teacher_acc, _, _ = validate(val_loader, model_t, criterion_cls, opt)
     print('teacher accuracy: ', teacher_acc)
 
-    test_loss_list = []
-    test_acc_list = []
-    train_loss_list = []
-    train_acc_list = []
     # routine
     for epoch in range(1, opt.epochs + 1):
 
@@ -300,8 +301,6 @@ def main():
 
         time1 = time.time()
         train_acc, train_loss = train(epoch, train_loader, module_list, criterion_list, optimizer, opt)
-        train_acc_list.append(train_acc)
-        train_loss_list.append(train_loss)
         time2 = time.time()
         print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
 
@@ -309,8 +308,6 @@ def main():
         logger.log_value('train_loss', train_loss, epoch)
 
         test_acc, tect_acc_top5, test_loss = validate(val_loader, model_s, criterion_cls, opt)
-        test_acc_list.append(test_acc)
-        test_loss_list.append(test_loss)
 
         logger.log_value('test_acc', test_acc, epoch)
         logger.log_value('test_loss', test_loss, epoch)
@@ -350,14 +347,6 @@ def main():
     }
     save_file = os.path.join(opt.save_folder, '{}_last.pth'.format(opt.model_s))
     torch.save(state, save_file)
-
-    fig = plt.figure(figsize=(20,10))
-    plt.title("learning curve")
-    plt.plot(train_acc_list, label='train')
-    plt.plot(test_acc_list, label='test')
-    plt.xlabel('num_epochs', fontsize=12)
-    plt.ylabel('accuracy', fontsize=12)
-    plt.legend(loc='best')
 
 
 if __name__ == '__main__':
